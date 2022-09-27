fc2aec57d2b7636bce0baa41742bdcf2fabe28df
Daisy Yoo <daisyhyoo@gmail.com>
2022-09-27 17:14:18 +0000

    Built DOM tree for viewing results, needs styling

--- js/main.js
+++ js/main.js
@@ -1,13 +1,27 @@
-var $flagContainer = document.querySelector('.flag-container');
-// var $allFlags = document.querySelectorAll('img');
-$flagContainer.addEventListener('click', cuisinePage);
+var $bodyContainer = document.querySelector('.body-container');
+var $header = document.querySelector('.header');
+var $flagsPage = document.querySelector('.flags-page');
+
+$bodyContainer.addEventListener('click', cuisinePage);
 
 function getCuisineData(name) {
   var xhr = new XMLHttpRequest();
   xhr.open('GET', 'https://api.spoonacular.com/recipes/complexSearch?apiKey=b35d81708b394cbfa180077a26661fe8&cuisine=' + name);
   xhr.responseType = 'json';
   xhr.addEventListener('load', function () {
-    // console.log(xhr.response);
+    var recipeListObject = xhr.response;
+    for (var i = 0; i < recipeListObject.results.length; i++) {
+      var row = $bodyContainer.appendChild(document.createElement('div'));
+      row.className = 'recipe-container col-lg-third col-sm-half';
+      var div = row.appendChild(document.createElement('div'));
+      div.className = 'recipe-box center col-direction';
+      var recipeButton = div.appendChild(document.createElement('button'));
+      // recipeButton.className = 'center';
+      var recipeImg = recipeButton.appendChild(document.createElement('img'));
+      var recipeTitle = div.appendChild(document.createElement('h5'));
+      recipeImg.setAttribute('src', recipeListObject.results[i].image);
+      recipeTitle.textContent = recipeListObject.results[i].title;
+    }
   });
   xhr.send();
 }
@@ -15,6 +29,8 @@ function getCuisineData(name) {
 function cuisinePage(event) {
   if (event.target.matches('.flag')) {
     var name = event.target.getAttribute('data-id');
+    $header.textContent = name + ' Dishes';
+    $flagsPage.className = 'hidden';
     getCuisineData(name);
   }
 }
